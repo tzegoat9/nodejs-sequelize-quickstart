a62194cae3b40fc47a2cec091f4a098dcd5a8704
tzegoat9 <tzegoat9@gmail.com>
2026-08-29 19:32:32 +0530

    Update locked test

--- omnigraph_locked_test.js
+++ omnigraph_locked_test.js
@@ -2,7 +2,7 @@
 const { sequelize, Settings } = require('./models');
 
 async function updateSettingsWithLock() {
-    return sequelize.transaction(async (t) => {
+    return Settings.transaction(async (t) => {
         await Settings.update({ value: 'test' }, { transaction: t });
     });
 }
