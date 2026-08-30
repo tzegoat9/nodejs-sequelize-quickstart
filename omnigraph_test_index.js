4fbfd2783e7609575b079385c3ef8b99cec4ff1c
tzegoat9 <tzegoat9@gmail.com>
2026-08-30 20:01:07 +0530

    Add fetchActiveUsers ts=1788100252

--- omnigraph_test_index.js
+++ omnigraph_test_index.js
@@ -1,5 +1,5 @@
 
-// Node Service Reading from Global Users (ts=1788100175)
+// Node Service Reading from Global Users (ts=1788100252)
 const GlobalUser = require('./models/GlobalUser');
 
 function fetchActiveUsers() {
